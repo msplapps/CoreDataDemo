cbfdab2c80ce87ffdc8d92bf1071f86e75b00e8e
Purushotham <xcode.ypr@gmail.com>
2017-08-30 12:19:23 +0530

    CoreData
    CoreData demo

---
+++ CoreDataTutorial/Extensions.swift
@@ -0,0 +1,41 @@
+//
+//  Extensions.swift
+//  CoreDataTutorial
+//
+//  Created by Harshvardhan Agarwal on 29/08/17.
+//  Copyright © 2017 James Rochabrun. All rights reserved.
+//
+
+import UIKit
+
+let imageCache = NSCache<NSString, UIImage>()
+
+extension UIImageView {
+    func loadImageUsingCacheWithURLString(_ URLString: String, placeHolder: UIImage?) {
+        self.image = nil
+        if let cachedImage = imageCache.object(forKey: NSString(string: URLString)) {
+            self.image = cachedImage
+            return
+        }
+        if let url = URL(string: URLString) {
+            URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                //print("RESPONSE FROM API: \(response)")
+                if error != nil {
+                    print("ERROR LOADING IMAGES FROM URL: \(error)")
+                    DispatchQueue.main.async {
+                        self.image = placeHolder
+                    }
+                    return
+                }
+                DispatchQueue.main.async {
+                    if let data = data {
+                        if let downloadedImage = UIImage(data: data) {
+                            imageCache.setObject(downloadedImage, forKey: NSString(string: URLString))
+                            self.image = downloadedImage
+                        }
+                    }
+                }
+            }).resume()
+        }
+    }
+}
---
+++ CoreDataTutorial/Photo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Photo+CoreDataClass.swift
+//  CoreDataTutorial
+//
+//  Created by Harshvardhan Agarwal on 29/08/17.
+//  Copyright © 2017 James Rochabrun. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Photo: NSManagedObject {
+
+}
